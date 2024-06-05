1e9afdf065a61ff23d5cd0a836387df96ae500ce
Valentine-Jingwa <119522550+Valentine-Jingwa@users.noreply.github.com>
2024-06-05 15:13:18 -0600

    things

--- src/pages/page.tsx
+++ src/pages/page.tsx
@@ -90,13 +90,13 @@ const Home = () => {
     }
   };
 
-  // if (isConnecting) {
-  //   return (
-  //     <div className="connecting-animation">
-  //       Connecting...
-  //     </div>
-  //   );
-  // }
+  if (isConnecting) {
+    return (
+      <div className="connecting-animation">
+        Connecting...
+      </div>
+    );
+  }
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-gray-50">
